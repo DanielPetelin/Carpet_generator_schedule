6d968cc8e05f05acf41170a66c4360b09a3f05a8
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-09 10:33:25 +0300

    add generator

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ var ctx = canvas.getContext('2d');
 var x = 0;
 var timer; 
 
-drowSin();
-
 // делаем функцию генератора ковров.
-function drowSin() {
-    y = Math.sin(x * 2676372653164317536715276155645176341723567153761746175327);
+document.getElementById('generator').onclick = function drowSin() {
+    var speed = document.getElementById('speed').value;
+    var code = document.getElementById('codeGeneration').value;
+    var sizeX = document.getElementById('sizeX').value;
+    var sizeY = document.getElementById('sizeY').value;
+
+    y = Math.sin(x * code);
     x = x + 0.05;
 
-    ctx.fillRect(1.5 * x, 200 + 200 * y, 3, 3);
-    timer = setTimeout(drowSin, 0.2);
+    ctx.fillRect(1.5 * x, 200 + 200 * y, sizeX, sizeY);
+    timer = setTimeout(drowSin, speed);
 };
