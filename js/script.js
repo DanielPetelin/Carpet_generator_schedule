7385a9f8a40e8c86bc4ac6ebc507ef361ac2f8ee
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-12 15:57:08 +0300

    add defolt and clear burron

--- js/script.js
+++ js/script.js
@@ -10,9 +10,29 @@ document.getElementById('generator').onclick = function drowSin() {
     var sizeX = document.getElementById('sizeX').value;
     var sizeY = document.getElementById('sizeY').value;
 
+    if(speed == '') {
+        speed = 1;
+    };
+
+    if(code == '') {
+        code = 1;
+    };
+
+    if(sizeX == '') {
+        sizeX = 1;
+    };
+
+    if(sizeY == '') {
+        sizeY = 1;
+    };
+
     y = Math.sin(x * code);
     x = x + 0.05;
 
     ctx.fillRect(1.5 * x, 200 + 200 * y, sizeX, sizeY);
     timer = setTimeout(drowSin, speed);
+};
+
+document.getElementById('clear').onclick = function() {
+    ctx.clearRect(0, 0, 1100, 400);
 };
